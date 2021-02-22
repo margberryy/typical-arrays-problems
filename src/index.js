637d8229fc155bce12e777830dfc43c2c5650a73
Marg <paerl.mag@gmail.com>
2021-02-22 15:37:25 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array) { 
+  if (array === undefined || array.length == 0){
+    return 0;
+  }else{
+  return Math.min.apply(Math, array);
+}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length == 0){
+    return 0;
+  }else{
+  return Math.max.apply(Math, array);
+}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length == 0){
+    return 0;
+  }else{
+  let sum = 0;
+  for(let i = 0; i < array.length; i++){
+    sum += array[i];
+  }
+  return sum / array.length;
+}
 }
